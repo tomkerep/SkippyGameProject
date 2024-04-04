cffdd3a717ee34dfb0e1c2405520f912e6b51328
Anna Jutz <annajutz@MacBook-Air-von-Anna.local>
2024-04-04 14:33:22 +0200

    slider + timer

---
+++ Assets/Images/hecke.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hecke : MonoBehaviour
+{
+
+    public GameObject hecke1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hecke1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
