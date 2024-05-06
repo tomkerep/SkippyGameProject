61e309e6a5ec434b1eeef047f9b4dce7d57d22ca
Anna Jutz <anna.jutz@hs-weingarten.de>
2024-05-06 10:29:22 +0200

    anzeige gamewon screen, fixes

--- Assets/Scripts/GameWonScreen.cs
+++ Assets/Scripts/GameWonScreen.cs
@@ -7,10 +7,40 @@ using UnityEngine.SceneManagement;
 public class GameWonScreen : MonoBehaviour
 {
     public TextMeshProUGUI pointsText;
+    public GameObject GameManager;
+    public GameObject Bau;
+
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
+
     public void Setup(int score)
     {
         gameObject.SetActive(true);
         pointsText.text = score.ToString() + " Nüsse gesammelt";
+        star1.SetActive(false);
+        star2.SetActive(false);
+        star3.SetActive(false);
+
+        timer2 timer = GameManager.GetComponent<timer2>();
+        BauInventory bauInventory = Bau.GetComponent<BauInventory>();
+
+        if(bauInventory.StoredNuts >= timer.NutsNeeded){
+            Debug.LogWarning("1 Stern");
+            star1.SetActive(true);
+        }
+
+        if(bauInventory.StoredNuts >= timer.NutsNeededStar2){
+            Debug.LogWarning("2 Sterne");
+            star2.SetActive(true);
+
+        }
+
+        if(bauInventory.StoredNuts >= timer.NutsNeededStar3){
+            Debug.LogWarning("3 Sterne");
+            star3.SetActive(true);
+        }
+
     }
 
     public void NextLevel()
