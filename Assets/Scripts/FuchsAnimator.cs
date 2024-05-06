7fc447d59c9058bab2f540b792a747786b6cfd9b
Anna Jutz <anna.jutz@hs-weingarten.de>
2024-05-06 13:34:57 +0200

    animation

---
+++ Assets/Scripts/FuchsAnimator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuchsAnimator : MonoBehaviour
+{
+/*
+    public Animator animator;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Player player = Player.GetComponent<Player>();
+
+        Vector3 inputVector = (Vector3.forward * Input.GetAxis("Vertical")) + (Vector3.right * Input.GetAxis("Horizontal"));
+
+        Vector3 animationVector = player.transform.InverseTransformDirection(inputVector);
+
+        var VelocityX = animationVector.x;
+        var VelocityZ = animationVector.z;
+ 
+        animator.SetFloat("x", VelocityX);
+        animator.SetFloat("z", VelocityZ);*/
+        
+    }
+
