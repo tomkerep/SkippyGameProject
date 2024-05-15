d885eac521486d260f4ec70b07b45ec2a1ce1704
Anna Jutz <anna.jutz@hs-weingarten.de>
2024-05-15 16:56:44 +0200

    counter am spieler, golden nut größer, bau gefixed, animation gefixed blablabla

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -5,21 +5,42 @@ using TMPro;
 
 public class InventoryUI : MonoBehaviour
 {
-    private TextMeshProUGUI nutText;
-    private Vector2 originalPosition;
+    private TextMeshPro nutText;
+    private Vector3 originalPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        nutText = GetComponent<TextMeshProUGUI>();  
-         originalPosition = nutText.rectTransform.anchoredPosition;
+        // Try to get the TextMeshPro component on the same GameObject
+        nutText = GetComponent<TextMeshPro>();
+        
+        // If not found, try to find it in the children
+        if (nutText == null)
+        {
+            nutText = GetComponentInChildren<TextMeshPro>();
+        }
+        
+        // Log an error if TextMeshPro component is still not found
+        if (nutText == null)
+        {
+            Debug.LogError("TextMeshPro component not found!");
+            return;
+        }
+
+        // Log success and continue with initialization
+        Debug.Log("TextMeshPro component found.");
+        originalPosition = nutText.transform.localPosition;
     }
 
     public void UpdateNutText(PlayerInventory playerInventory)
     {
+        if (nutText == null)
+        {
+            Debug.LogError("TextMeshPro component not assigned!");
+            return;
+        }
+
         nutText.text = playerInventory.NumberOfNuts.ToString() + "/" + playerInventory.MaxNumberOfNuts.ToString();
     }
-
-    
-
 }
+
