0fb73977b1799d2a7646a2529a9bd4b02647dde6
Anna Jutz <anna.jutz@hs-weingarten.de>
2024-05-16 16:52:15 +0200

    damage screen

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -9,20 +9,26 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject NutText;
     public Transform respawnPoint;
     public AudioClip hit_sound;
+    //public GameObject PostProcessingGO;
+    public TakeDamage takeDamage;
 
     private void OnCollisionEnter(Collision other)
     {
-        PlayerInventory playerInventory = other.gameObject.GetComponent<PlayerInventory>();
-        InventoryUI inventory = NutText.GetComponent<InventoryUI>();
-
         if (other.gameObject.CompareTag("Player"))
         {
+        PlayerInventory playerInventory = other.gameObject.GetComponent<PlayerInventory>();
+        InventoryUI inventory = NutText.GetComponent<InventoryUI>();
+        //TakeDamage takeDamage = PostProcessingGO.GetComponent<TakeDamage>();
+
+         StartCoroutine(takeDamage.TakeDamageEffect());
             playerInventory.NumberOfNuts = 0;
             Player.transform.position = respawnPoint.position;
             // Soundeffekt abspielen
             AudioManager.instance.PlaySFX(hit_sound);
 
             inventory.UpdateNutText(playerInventory); 
+
+            
             
         }
     }
